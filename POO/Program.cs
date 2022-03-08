6d61e5c10c5c6a53fa629049a2af98772473795d
ygor borges areda <ygorareda@gmail.com>
2022-03-08 14:26:20 -0300

    Adicionando o código

---
+++ POO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+namespace POO
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string palavra = ReadLine();
+
+            string palavraInvertida = new string(palavra.Reverse().ToArray());
+
+            if (palavra.ToLower() == palavraInvertida.ToLower())
+            {
+                WriteLine(palavra);
+                WriteLine(palavraInvertida);
+                WriteLine("É um palindromo");
+            }
+            else
+            {
+                WriteLine(palavra);
+                WriteLine(palavraInvertida);
+                WriteLine("Não é um palindromo");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
